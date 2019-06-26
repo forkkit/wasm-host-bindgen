57117e3ec45ff9b981fd17ac3a6e4528a520fd70
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2019-06-26 16:39:30 +0200

    feat(decoder-common) Simplify the `Decoder` trait.

--- lib/decoder-c/src/lib.rs
+++ lib/decoder-c/src/lib.rs
@@ -13,29 +13,12 @@ impl Decoder {
 }
 
 impl common::Decoder for Decoder {
-    fn decode(&self, options: Options) -> Result<(), &'static str> {
+    fn decode(options: Options) -> Result<(), &'static str> {
         options
             .webassembly_module
             .parse(|bindings: &WebidlBindings| {
-                println!("{:?}", bindings);
+                println!("{:#?}", bindings);
             });
-        /*
-        match get_binding_section(&options.webassembly_module) {
-            Some(section) => {
-                /*
-                let ids_to_indices = IdsToIndices::default();
-                let reader = section.data(&ids_to_indices).into_owned();
-                let indices_to_ids = IndicesToIds::default();
-
-                println!("{:?}", reader);
-                println!("{:?}", decode(&indices_to_ids, &mut reader.as_slice()));
-                 */
-                println!("{:?}", section);
-            }
-
-            None => return Err("No binding custom section found in the given module."),
-        }
-        */
 
         Ok(())
     }
--- lib/decoder-common/src/lib.rs
+++ lib/decoder-common/src/lib.rs
@@ -8,14 +8,14 @@ pub use walrus;
 pub use wasm_webidl_bindings;
 
 pub trait Decoder {
-    fn decode(&self, _: Options) -> Result<(), &'static str>;
+    fn decode(_: Options) -> Result<(), &'static str>;
 }
 
-pub fn decode<D>(options: Options, decoder: D) -> Result<(), &'static str>
+pub fn decode<D>(options: Options) -> Result<(), &'static str>
 where
     D: Decoder,
 {
-    decoder.decode(options)
+    D::decode(options)
 }
 
 pub const BINDING_SECTION_NAME: &'static str = "webidl-bindings";
