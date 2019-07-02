32ecd9eddb5a3ba910c785dc1ba8d825ccc9cd95
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2019-07-02 11:51:49 +0200

    feat(decoder-c) Reorganize code, and start type checking.

--- lib/decoder-c/src/lib.rs
+++ lib/decoder-c/src/lib.rs
@@ -6,8 +6,9 @@ use wasm_xbindgen_decoder_common::{
     options::Options,
     walrus::{ExportItem, Function, FunctionId, FunctionKind, LocalFunction, Module, Type, TypeId},
     wasm_webidl_bindings::ast::{
-        Bind, ExportBinding, FunctionBindingId, OutgoingBindingMap, WebidlBindings, WebidlFunction,
-        WebidlTypeId, WebidlTypeRef,
+        Bind, ExportBinding, FunctionBindingId, FunctionBindings, ImportBinding,
+        OutgoingBindingExpression, OutgoingBindingExpressionUtf8CStr, OutgoingBindingMap,
+        WebidlBindings, WebidlFunction, WebidlTypeId, WebidlTypeRef, WebidlTypes,
     },
 };
 
@@ -22,11 +23,17 @@ impl common::Decoder for Decoder {
 }
 
 fn decode(module: &Module, webidl_bindings: &WebidlBindings, mut writer: &File) {
-    webidl_bindings.codegen(&mut CodegenContext {
-        module,
-        webidl_bindings,
-        writer: &mut writer,
-    });
+    let binds = &webidl_bindings.binds;
+
+    for bind in binds.iter().filter_map(|index| binds.get(*index)) {
+        bind.codegen(&mut CodegenContext {
+            module,
+            writer: &mut writer,
+            bind,
+            bindings: &webidl_bindings.bindings,
+            types: &webidl_bindings.types,
+        });
+    }
 }
 
 trait Codegen {
@@ -35,8 +42,10 @@ trait Codegen {
 
 struct CodegenContext<'a> {
     module: &'a Module,
-    webidl_bindings: &'a WebidlBindings,
     writer: &'a mut dyn Write,
+    bind: &'a Bind,
+    bindings: &'a FunctionBindings,
+    types: &'a WebidlTypes,
 }
 
 impl<'a> CodegenContext<'a> {
@@ -49,100 +58,117 @@ impl<'a> CodegenContext<'a> {
     }
 }
 
-impl Codegen for WebidlBindings {
-    fn codegen(&self, context: &mut CodegenContext) {
-        // Iterate over `bind <wasm> <webidl>` statements.
-        for bind in self.binds.iter().filter_map(|index| self.binds.get(*index)) {
-            bind.codegen(context);
-        }
-    }
-}
-
+// Represent the `bind <wasm-function> <webidl-function-binding>` statement.
 impl Codegen for Bind {
     fn codegen(&self, context: &mut CodegenContext) {
-        dbg!(self);
         let module = context.module;
-        let bindings = &context.webidl_bindings.bindings;
+
+        /*
+        // Get the WebIDL function type.
+         */
 
         // Bound Wasm function.
         let wasm_function = context.get_function(self.func);
 
-        // Get the `func-binding` statement.
-        let function_binding = bindings
-            .get::<ExportBinding>(ExportBinding::wrap(self.binding))
-            .expect("Web IDL AST is malformed.");
-
-        // Get the bound Wasm function type.
-        let function_binding_wasm_type = context.get_type(function_binding.wasm_ty);
-
-        // Get the WebIDL function type.
-        let function_binding_webidl_type = match function_binding.webidl_ty {
-            WebidlTypeRef::Id(id) => context
-                .webidl_bindings
-                .types
-                .get::<WebidlFunction>(WebidlFunction::wrap(id)),
-
-            _ => panic!("ho no"),
-        };
-
-        dbg!(function_binding);
-        dbg!(function_binding_wasm_type);
-        dbg!(function_binding_webidl_type);
-        dbg!(wasm_function);
-
-        match &wasm_function.kind {
-            // The bound Wasm function is an imported function.
-            FunctionKind::Import(imported_function) => {
-                let wasm_import = module.imports.get(imported_function.import);
-                dbg!(wasm_import);
-            }
-
-            // The bound Wasm function is an exported function.
+        // Is it a binding to an export, and is the Wasm function an export?
+        if let (
+            Some(export_binding),
             FunctionKind::Local(LocalFunction {
                 ty: exported_function_type_id,
                 ..
-            }) => {
-                if function_binding_wasm_type != module.types.get(*exported_function_type_id) {
-                    panic!("WebIDL type for the Wasm function, and the actual Wasm function type differ.");
-                }
+            }),
+        ) = (
+            context
+                .bindings
+                .get::<ExportBinding>(ExportBinding::wrap(self.binding)),
+            &wasm_function.kind,
+        ) {
+            let export_binding_wasm_type = context.get_type(export_binding.wasm_ty);
+            let exported_function_type = module.types.get(*exported_function_type_id);
 
-                let wasm_export_name = module
-                    .exports
-                    .iter()
-                    .find_map(|export| match export.item {
-                        ExportItem::Function(function_id) if function_id == wasm_function.id() => {
-                            dbg!(&export.name);
-
-                            Some(&export.name)
-                        }
-
-                        _ => None,
-                    })
-                    .expect("Failed to find an exported function.");
-
-                write!(
-                    context.writer,
-                    r#"
-exports_{name} = |instance: Instance| {{
-    let pointer: u32 = instance.exports["{name}"]();
-    let byte_string = ByteString::new();
-    let memory = instance.memory.view::<u8>();
-
-    match memory.position(|b| b != 0) {{
-        Some(offset) => ByteString::from(memory[pointer..offset]),
-        None => ByteString::from(memory[pointer..]),
-    }}
-}}
-"#,
-                    name = wasm_export_name
-                )
-                .unwrap();
-
-                function_binding.result.codegen(context);
+            if export_binding_wasm_type != exported_function_type {
+                panic!(
+                    "WebIDL type for the Wasm function, and the actual Wasm function type differ."
+                );
             }
 
-            _ => panic!("Not implemented yet."),
+            export_binding.codegen(context);
         }
+        // Is it a binding to an import, and is the Wasm function an import?
+        else if let (Some(import_binding), FunctionKind::Import(imported_function)) = (
+            context
+                .bindings
+                .get::<ImportBinding>(ImportBinding::wrap(self.binding)),
+            &wasm_function.kind,
+        ) {
+            let wasm_import = context.module.imports.get(imported_function.import);
+
+            dbg!(import_binding);
+            dbg!(wasm_import);
+        }
+        // Mismatch.
+        else {
+            unimplemented!()
+        }
+    }
+}
+
+impl Codegen for ExportBinding {
+    fn codegen(&self, context: &mut CodegenContext) {
+        assert!(self.params.bindings.is_empty());
+        assert!(self.result.bindings.len() == 1);
+
+        dbg!(self);
+
+        // Get the `type` statement that represents the Web IDL binding function.
+        let export_type = match self.webidl_ty {
+            WebidlTypeRef::Id(id) => context
+                .types
+                .get::<WebidlFunction>(WebidlFunction::wrap(id)),
+            _ => unimplemented!(),
+        };
+
+        // Check that the `type` and the `func-binding` types match.
+        match (export_type, &self.result.bindings[0]) {
+            (
+                Some(WebidlFunction {
+                    result: Some(left_type),
+                    ..
+                }),
+                OutgoingBindingExpression::Utf8CStr(OutgoingBindingExpressionUtf8CStr {
+                    ty: right_type,
+                    ..
+                }),
+            ) => {
+                if left_type != right_type {
+                    panic!("Type mimatch between `type` and `func-binding`.");
+                }
+            }
+
+            _ => unimplemented!(),
+        };
+
+        let wasm_function_id = context.get_function(context.bind.func).id();
+        let wasm_export = context
+            .module
+            .exports
+            .iter()
+            .find(|export| match export.item {
+                ExportItem::Function(function_id) if function_id == wasm_function_id => true,
+                _ => false,
+            })
+            .expect("Bound exported function is not found.");
+
+        dbg!(wasm_export);
+
+        write!(
+            context.writer,
+            r#"
+export_{name} = |instance: Instance| {{
+    let pointer "#,
+            name = wasm_export.name
+        )
+        .unwrap();
     }
 }
 
