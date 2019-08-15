6ea05d4761f8cbcec56f35268063e1688b3bd92d
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2019-08-15 15:26:26 +0200

    fix(decoder) Remove `unwrap` and update the code.

--- lib/decoder-common/src/module.rs
+++ lib/decoder-common/src/module.rs
@@ -47,15 +47,18 @@ impl Module {
                     .expect("No Web IDL bindings custom section in this WebAssembly module.");
 
                 let data = section.data(&Default::default());
-                let bindings = decode(indices_to_ids, &data).unwrap();
+                let bindings = decode(indices_to_ids, &data).expect("Failed to decode bindings.");
 
-                let mut writer = File::create(output.clone()).unwrap();
+                let mut writer =
+                    File::create(output.clone()).expect("Failed to write the decoded bindings.");
 
                 handler(prelude, &module, &bindings, &mut writer);
 
                 Ok(())
             });
 
-        self.module_configuration.parse(&self.module_bytes).unwrap();
+        self.module_configuration
+            .parse(&self.module_bytes)
+            .expect("Failed to parse the WebAssembly module.");
     }
 }
